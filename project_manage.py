8c3f6ae3004d42e8c9bf3bb65daec99d450152b6
Phasathat Jaruchitsophon <phasathat.j@ku.th>
2023-12-17 01:32:59 +0700

    so many to do

--- project_manage.py
+++ project_manage.py
@@ -18,18 +18,22 @@ def initializing():
     """
     csv_login = csv1.read_csv('login.csv')
     csv_person = csv1.read_csv('persons.csv')
+    csv_project = csv1.read_csv('project.csv')
+    csv_advisor = csv1.read_csv('Advisor_pending_request.csv')
+    csv_member = csv1.read_csv('Member_pending_request.csv')
     login_table = Table('login', csv_login)
     person_table = Table('persons', csv_person)
+    project_table = Table('project', csv_project)
+    advisor_table = Table('persons', csv_advisor)
+    member_table = Table('persons', csv_member)
     DB.insert(login_table)
     DB.insert(person_table)
-    for i in csv_login:
-        print(i['username'])
+    DB.insert(project_table)
+    DB.insert(advisor_table)
+    DB.insert(member_table)
 
 
-# initializing()
-# define a function called login
-
-def login():
+def login_base():
     username = input('Enter Username: ')
     password = input('Enter Password: ')
     data_login = DB.database[0].table
@@ -47,12 +51,46 @@ def login():
 
 # define a function called exit
 def exit():
-    with open('persons.csv.csv', 'w', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerow(['ID', 'username', 'password', 'role'])
-        for each in DB.database[0].table:
-            writer.writerow(each.values())
+    login = open('login.csv', 'w')
+    login_writer = csv.writer(login)
+    login_writer.writerow(['ID', 'username', 'password', 'role'])
+    for each in DB.database[0].table:
+        login_writer.writerow(each.values())
+    login.close()
 
+    person = open('persons.csv', 'w')
+    person_writer = csv.writer(person)
+    person_writer.writerow(['ID', 'first', 'last', 'type'])
+    for each in DB.database[1].table:
+        person_writer.writerow(each.values())
+    person.close()
+
+    project = open('project.csv', 'w')
+    project_writer = csv.writer(project)
+    project_writer.writerow(['ID', 'first', 'last', 'type'])
+    for each in DB.database[1].table:
+        person_writer.writerow(each.values())
+    person.close()
+
+    advisor_pending = open('Advisor_pending_request.csv', 'w')
+    advisor_pending_writer = csv.writer(advisor_pending)
+    advisor_pending_writer.writerow(['ID', 'first', 'last', 'type'])
+    for each in DB.database[1].table:
+        person_writer.writerow(each.values())
+    person.close()
+
+    member_pending = open('member_pending_request.csv', 'w')
+    member_pending_writer = csv.writer(member_pending)
+    member_pending_writer.writerow(['ID', 'first', 'last', 'type'])
+    for each in DB.database[1].table:
+        person_writer.writerow(each.values())
+    person.close()
+
+
+    print('\n Program Exit')
+
+def project_id():
+    pass
 
 # here are things to do in this function:
 # write out all the tables that have been modified to the corresponding csv files
@@ -64,22 +102,22 @@ def exit():
 # make calls to the initializing and login functions defined above
 
 initializing()
-val = login()
+val = login_base()
 
 # based on the return value for login, activate the code that performs activities according to the role defined for that person_id
 
-if val[1] == 'admin':
-    pass
-elif val[1] == 'student':
-    pass
-elif val[1] == 'member':
-    pass
-elif val[1] == 'lead':
-    pass
-elif val[1] == 'faculty':
-    pass
-elif val[1] == 'advisor':
-    pass
+# if val[1] == 'admin':
+#     pass
+# elif val[1] == 'student':
+#     pass
+# elif val[1] == 'member':
+#     pass
+# elif val[1] == 'lead':
+#     pass
+# elif val[1] == 'faculty':
+#     pass
+# elif val[1] == 'advisor':
+#     pass
 
 # once everyhthing is done, make a call to the exit function
 exit()
